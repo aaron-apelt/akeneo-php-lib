2eb10ad86d9dcc8128084c01a1964141c3ee84d8
aaron-apelt <190078237+aaron-apelt@users.noreply.github.com>
2025-06-11 16:32:39 +0200

    feat: Add serializer

---
+++ src/Serializer/Serializer.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AkeneoLib\Serializer;
+
+use AkeneoLib\Exception\SerializationException;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer as BaseSerializer;
+use Throwable;
+
+/**
+ * A generous serializer implementation using Symfony Serializer.
+ */
+class Serializer implements SerializerInterface
+{
+    private BaseSerializer $serializer;
+
+    public function __construct(?BaseSerializer $serializer = null)
+    {
+        if ($serializer) {
+            $this->serializer = $serializer;
+        } else {
+
+            $normalizers = [
+                new ArrayDenormalizer,
+                new ObjectNormalizer(
+                    null,
+                    new CamelCaseToSnakeCaseNameConverter,
+                    null,
+                    new ReflectionExtractor),
+            ];
+
+            $this->serializer = new BaseSerializer($normalizers);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function denormalize(array $data, string $format): mixed
+    {
+        try {
+            return $this->serializer->denormalize($data, $format);
+        } catch (Throwable $e) {
+            throw new SerializationException(
+                sprintf('Failed to denormalize data to class "%s": %s', $format, $e->getMessage()),
+                previous: $e
+            );
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function normalize(array|object $data): array
+    {
+        try {
+            return $this->serializer->normalize($data);
+        } catch (Throwable $e) {
+            throw new SerializationException(
+                sprintf('Failed to normalize data: %s', $e->getMessage()),
+                previous: $e
+            );
+        }
+    }
+}
