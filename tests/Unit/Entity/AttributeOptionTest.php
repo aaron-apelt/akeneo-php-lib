01840104e3cf06fc3c85869df64190e129dc0377
aaron.apelt <190078237+aaron-apelt@users.noreply.github.com>
2026-01-24 18:41:54 +0100

    feat: add additional attribute option properties

--- tests/Unit/Entity/AttributeOptionTest.php
+++ tests/Unit/Entity/AttributeOptionTest.php
@@ -28,3 +28,37 @@ describe('labels management', function () {
         expect($option->getLabels())->toBe($labels);
     });
 });
+
+describe('attribute management', function () {
+    it('can get the attribute (initially null)', function () {
+        $option = new AttributeOption('red');
+        expect($option->getAttribute())->toBeNull();
+    });
+
+    it('can set the attribute', function () {
+        $option = new AttributeOption('red')->setAttribute('color');
+        expect($option->getAttribute())->toBe('color');
+    });
+
+    it('can set the attribute to null', function () {
+        $option = new AttributeOption('red')->setAttribute('color')->setAttribute(null);
+        expect($option->getAttribute())->toBeNull();
+    });
+});
+
+describe('sort order management', function () {
+    it('can get the sort order (initially null)', function () {
+        $option = new AttributeOption('red');
+        expect($option->getSortOrder())->toBeNull();
+    });
+
+    it('can set the sort order', function () {
+        $option = new AttributeOption('red')->setSortOrder(10);
+        expect($option->getSortOrder())->toBe(10);
+    });
+
+    it('can set the sort order to null', function () {
+        $option = new AttributeOption('red')->setSortOrder(5)->setSortOrder(null);
+        expect($option->getSortOrder())->toBeNull();
+    });
+});
