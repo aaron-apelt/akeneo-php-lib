5cfcc734d41cb1766c21a068bc58a014766d93d8
aaron.apelt <190078237+aaron-apelt@users.noreply.github.com>
2026-01-24 18:37:42 +0100

    feat: add additional attribute group properties

--- src/Entity/AttributeGroup.php
+++ src/Entity/AttributeGroup.php
@@ -8,6 +8,10 @@ class AttributeGroup
 {
     private ?array $labels;
 
+    private ?int $sortOrder;
+
+    private ?array $attributes;
+
     public function __construct(private string $code) {}
 
     public function getCode(): string
@@ -30,6 +34,30 @@ class AttributeGroup
     public function setLabels(?array $labels): self
     {
         $this->labels = $labels;
+
+        return $this;
+    }
+
+    public function getSortOrder(): ?int
+    {
+        return $this->sortOrder ?? null;
+    }
+
+    public function setSortOrder(?int $sortOrder): self
+    {
+        $this->sortOrder = $sortOrder;
+
+        return $this;
+    }
+
+    public function getAttributes(): ?array
+    {
+        return $this->attributes ?? null;
+    }
+
+    public function setAttributes(?array $attributes): self
+    {
+        $this->attributes = $attributes;
 
         return $this;
     }
--- tests/Unit/Entity/AttributeGroupTest.php
+++ tests/Unit/Entity/AttributeGroupTest.php
@@ -34,3 +34,38 @@ describe('labels management', function () {
         expect($group->getLabels())->toBeNull();
     });
 });
+
+describe('sort order management', function () {
+    it('can get the sort order (initially null)', function () {
+        $group = new AttributeGroup('technical');
+        expect($group->getSortOrder())->toBeNull();
+    });
+
+    it('can set the sort order', function () {
+        $group = new AttributeGroup('technical')->setSortOrder(2);
+        expect($group->getSortOrder())->toBe(2);
+    });
+
+    it('can set the sort order to null', function () {
+        $group = new AttributeGroup('technical')->setSortOrder(5)->setSortOrder(null);
+        expect($group->getSortOrder())->toBeNull();
+    });
+});
+
+describe('attributes management', function () {
+    it('can get the attributes (initially null)', function () {
+        $group = new AttributeGroup('technical');
+        expect($group->getAttributes())->toBeNull();
+    });
+
+    it('can set the attributes', function () {
+        $attributes = ['meta_title', 'meta_description', 'seo_keywords'];
+        $group = new AttributeGroup('technical')->setAttributes($attributes);
+        expect($group->getAttributes())->toBe($attributes);
+    });
+
+    it('can set the attributes to null', function () {
+        $group = new AttributeGroup('technical')->setAttributes(['name'])->setAttributes(null);
+        expect($group->getAttributes())->toBeNull();
+    });
+});
