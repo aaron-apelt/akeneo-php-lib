5d96615f15f924b577cb8d261749f4a7d9da364d
aaron.apelt <190078237+aaron-apelt@users.noreply.github.com>
2026-01-24 18:50:12 +0100

    feat: add additional channel properties

--- tests/Unit/Entity/ChannelTest.php
+++ tests/Unit/Entity/ChannelTest.php
@@ -54,6 +54,24 @@ describe('currencies management', function () {
     });
 });
 
+describe('locales management', function () {
+    it('can get the locales (initially null)', function () {
+        $channel = new Channel('ecommerce');
+        expect($channel->getLocales())->toBeNull();
+    });
+
+    it('can set the locales', function () {
+        $locales = ['en_US', 'fr_FR', 'de_DE'];
+        $channel = new Channel('ecommerce')->setLocales($locales);
+        expect($channel->getLocales())->toBe($locales);
+    });
+
+    it('can set the locales to null', function () {
+        $channel = new Channel('ecommerce')->setLocales(['en_US'])->setLocales(null);
+        expect($channel->getLocales())->toBeNull();
+    });
+});
+
 describe('category tree management', function () {
     it('can get the category tree (initially null)', function () {
         $channel = new Channel('ecommerce');
@@ -68,5 +86,23 @@ describe('category tree management', function () {
     it('can set the category tree to null', function () {
         $channel = new Channel('ecommerce')->setCategoryTree('master')->setCategoryTree(null);
         expect($channel->getCategoryTree())->toBeNull();
+    });
+});
+
+describe('conversion units management', function () {
+    it('can get the conversion units (initially null)', function () {
+        $channel = new Channel('ecommerce');
+        expect($channel->getConversionUnits())->toBeNull();
+    });
+
+    it('can set the conversion units', function () {
+        $conversionUnits = ['weight' => 'KILOGRAM', 'length' => 'CENTIMETER'];
+        $channel = new Channel('ecommerce')->setConversionUnits($conversionUnits);
+        expect($channel->getConversionUnits())->toBe($conversionUnits);
+    });
+
+    it('can set the conversion units to null', function () {
+        $channel = new Channel('ecommerce')->setConversionUnits(['weight' => 'GRAM'])->setConversionUnits(null);
+        expect($channel->getConversionUnits())->toBeNull();
     });
 });
