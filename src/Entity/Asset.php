63b1dc16c1a778a9f96e4a6522fbdb273df6cc50
aaron.apelt <190078237+aaron-apelt@users.noreply.github.com>
2026-01-24 17:14:18 +0100

    feat: add additional asset properties

--- src/Entity/Asset.php
+++ src/Entity/Asset.php
@@ -8,6 +8,8 @@ class Asset
 {
     use ValuesTrait;
 
+    private ?string $assetFamilyCode;
+
     public function __construct(private string $code) {}
 
     public function getCode(): string
@@ -18,6 +20,18 @@ class Asset
     public function setCode(string $code): self
     {
         $this->code = $code;
+
+        return $this;
+    }
+
+    public function getAssetFamilyCode(): ?string
+    {
+        return $this->assetFamilyCode ?? null;
+    }
+
+    public function setAssetFamilyCode(?string $assetFamilyCode): self
+    {
+        $this->assetFamilyCode = $assetFamilyCode;
 
         return $this;
     }
